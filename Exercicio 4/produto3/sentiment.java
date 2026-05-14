657202b083abf0cbdd393463e0406358cac315ff
letfaarias <legoncalveslgf@gmail.com>
2026-05-14 20:57:34 -0300

    criação sentiment.java

---
+++ Exercicio 4/produto3/sentiment.java
@@ -0,0 +1,48 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class sentiment {
+
+    private static final String ENDPOINT = System.getenv("AZURE_LANGUAGE_ENDPOINT");
+    private static final String KEY = System.getenv("AZURE_LANGUAGE_KEY");
+
+    public static void main(String[] args) throws Exception {
+        String text = "gostei muito do atendimento, foi rápido e eficiente.";
+
+        String url = ENDPOINT + "/text/analytics/v3.2/sentiment?opinionMining=true";
+
+        String bodyJson =
+            "{\n" +
+            "  \"documents\": [\n" +
+            "    {\n" +
+            "      \"id\": \"1\",\n" +
+            "      \"language\": \"pt-BR\",\n" +
+            "      \"text\": \"" + escapeJson(text) + "\"\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .header("Ocp-Apim-Subscription-Key", KEY)
+                .POST(HttpRequest.BodyPublishers.ofString(bodyJson))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> resp = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("HTTP " + resp.statusCode());
+        System.out.println(resp.body());
+    }
+
+    private static String escapeJson(String s) {
+        return s.replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\t", "\\t");
+    }
+}
